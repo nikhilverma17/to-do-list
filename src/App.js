8aec8dbcbbadcaa813d7ca1ba247cf9fb73052dd
Nikhil Verma <63592210+nikhilverma17@users.noreply.github.com>
2023-08-10 16:51:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  function change(event) {
+    const newValue = event.target.value;
+    setInput(newValue);
+  }
+
+  function addItem() {
+    setItems(prevValue => {
+      return [...prevValue, input];
+    });
+    setInput("");
+  }
+
+  function deleteItem(index) {
+    setItems(prevValue => {
+      const updatedItems = [...prevValue];
+      updatedItems.splice(index, 1);
+      return updatedItems;
+    });
+  }
+
+  function reset() {
+    setItems([]);
+    setInput("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={change} value={input} type="text" placeholder="Add a new item..." />
+        <br />
+        <button className="add-button" onClick={addItem}>
+          <span>Add</span>
+        </button>
+        <button className="reset-button" onClick={reset}>
+          <span>Reset</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <li key={index}>
+              {todoItem}
+              <button className="delete-button" onClick={() => deleteItem(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+function Footer(){
+    return(
+        <footer>
+        <p class="p-footer"><i class="icon fa-solid fa-copyright fa-lg"></i> Nikhil Verma</p>
+          <div class="footer">
+          <a href="https://github.com/nikhilverma17" target="_blank"><i
+              class="icon1 fa-brands fa-github  fa-lg"></i></a>
+          <a href="mailto:iamvermanikhil@gmail.com" target="_blank"><i
+              class="icon1 fa-solid fa-envelope fa-lg"></i></a>
+          <a href="https://www.linkedin.com/in/nihkil-verma-465848139/" target="_blank"><i
+              class="icon1 fa-brands fa-linkedin fa-lg"></i></a>
+          <a href="https://twitter.com/thenikhil_verma" target="_blank"><i
+              class="icon1 fa-brands fa-twitter fa-lg"></i></a>
+          <a href="https://www.instagram.com/thevermanikhil/" target="_blank"><i
+              class="icon1 fa-brands fa-instagram fa-lg"></i></a>
+          
+        </div>
+        
+        </footer>
+    )
+}
+export default Footer
